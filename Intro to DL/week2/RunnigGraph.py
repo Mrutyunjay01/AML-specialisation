23de4598915da1543ae18a15eb44588a888e34fd
Mrutyunjay01 <mrutyunjay.tutulu1021@gmail.com>
2020-05-12 21:07:24 +0530

    Creating and running a graph done

---
+++ Intro to DL/week2/RunnigGraph.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+# tf.session encapsulates the env in which
+# tf.operation objects are execud and
+# tf.tensor objects are evaluated
+
+# craete a session
+sess = tf.InteractiveSession()
+
+# define a graph
+a = tf.constant(5.0)
+b = tf.constant(6.0)
+c = a * b
+
+# running a graph
+print(c)
+print(sess.run(c))
